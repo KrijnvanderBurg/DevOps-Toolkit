3eb217381f1ebca0efb1b66d3101377e5a49ae80
Krijn van der Burg <krijnvdburg@protonmail.com>
2024-11-15 00:31:50 +0100

    fix

---
+++ .tools/commands/mypy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Default values
+target_path="${1:-$PWD}"  # Default to current directory if not specified
+config_filepath="${2:-$PWD/../.tools/.mypy.ini}"  # Default config file path if not specified
+
+# Parse named parameters using a for loop
+for i in "$@"; do
+  case $i in
+    --target_path=*) target_path="${i#*=}" ;;
+    --config_filepath=*) config_filepath="${i#*=}" ;;
+    *) echo "Unknown option: $i" ;;  # Handle invalid arguments
+  esac
+done
+
+pipx install mypy --quiet
+
+# Print the current mypy version
+echo "mypy version:"
+mypy --version
+
+# Scanning the target folder with mypy
+echo "Config file: $config_filepath"
+echo "Scanning folder: $target_path"
+
+mypy "$target_path" \
+  --config-file "$config_filepath"
+
+echo "Finished."
