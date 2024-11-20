629f7204d8a2726ac51bbbfb605086c14b155081
Krijn van der Burg <krijnvdburg@protonmail.com>
2024-11-20 23:38:46 +0000

    fix

--- lakehouse/src/lakehouse/main.py
+++ lakehouse/src/lakehouse/main.py
@@ -1,20 +1,51 @@
-from cdktf import App, TerraformStack
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
 
-from ckdktf_azure import AzureProvider
-
-import pyspark
-import cdktf
-from cdktf import Terra
+from constructs import Construct
+from cdktf import App, TerraformStack, TerraformOutput, Token
+from imports.azurerm.provider import AzurermProvider
+from imports.azurerm.resource_group import ResourceGroup
+from imports.azurerm.virtual_network import VirtualNetwork
 
 
 class MyStack(TerraformStack):
-    def __init__(self, scope: Construct, id: str):
-        super().__init__(scope, id)
+    def __init__(self, scope: Construct, ns: str):
+        super().__init__(scope, ns)
 
-        AzureProvider(self, "azure", region="west-europe", features=[{}])
+        # define resources here
+        loca = "West Europe"
+        add_space = ["10.12.0.0/27"]
+        rg_name = "example-rg"
+        tag = {
+            "ENV": "Dev",
+            "PROJECT": "AZ_TF"
+        }
+
+        AzurermProvider(self, "Azurerm",
+                        features={}
+                        )
+
+        example_rg = ResourceGroup(self, 'example-rg',
+                                   name=rg_name,
+                                   location=loca,
+                                   tags=tag
+                                   )
+
+        example_vnet = VirtualNetwork(self, 'example_vnet',
+                                      depends_on=[example_rg],
+                                      name="example_vnet",
+                                      location=loca,
+                                      address_space=add_space,
+                                      resource_group_name=Token().as_string(example_rg.name),
+                                      tags=tag
+                                      )
+
+        TerraformOutput(self, 'vnet_id',
+                        value=example_vnet.id
+                        )
 
 
 app = App()
-MyStack(app, "lakehouse")
+MyStack(app, "python-azure")
 
 app.synth()
