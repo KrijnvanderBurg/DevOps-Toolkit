e7c75fc42824cdcf2432776def410421b0412237
Krijn van der Burg <krijnvanderburg@protonmail.com>
2025-01-09 18:10:15 +0100

    test

--- hello_world/tests/unit/test_always_pass.py
+++ hello_world/tests/unit/test_always_pass.py
@@ -4,11 +4,14 @@ from hello_world.__main__ import main
 def test_always_pass1() -> None:
     assert True
 
+
 def test_always_pass2() -> None:
     assert True
+
 
 def test_always_pass3() -> None:
     assert True
 
+
 def test_main() -> None:
-    main()
+    main(filepath="path/to/test.txt")
