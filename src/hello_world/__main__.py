02db5c5c8ee17064b4dd6ae70fdc9a3917d71edb
KrijnvanderBurg <32356726+KrijnvanderBurg@users.noreply.github.com>
2025-07-14 22:41:07 +0000

    w

--- src/hello_world/__main__.py
+++ src/hello_world/__main__.py
@@ -1,39 +1,6 @@
-"""
-hello_world
-================
-
-This module contains the main entry point for the Hello World application.
-
-note: Sphinx does not add __main__.py or __init__.py to the docs output.
-
-Functions
----------
-main()
-    Prints filepath argument to the console.
-"""
-
-from argparse import ArgumentParser
-
-
-def main(filepath: str) -> None:
-    """
-    Main entry point for the Hello World application.
-
-    Prints 'Hello, World!' to the console.
-
-    Args:
-        filepath (str): The path to a file.
-    """
-
-    apple: str = 5  # intended error to show in Problems tab or vscode tasks.
-
-    if filepath != "":
-        print(filepath)
-
+import subprocess
 
 if __name__ == "__main__":
-    parser = ArgumentParser()
-    parser.add_argument("--filepath", required=True, type=str)
-    args = parser.parse_args()
-
-    main(filepath=args.filepath)
+    print("Hello, World!")
+    string: str = 42
+    subprocess.run(["/bin/ls", "-l"], check=True)
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,17 +1,31 @@
-import pytest
-import json
-import tempfile
-import os
-
 """
 Fixtures to reuse.
 """
 
+import json
+import tempfile
+from collections.abc import Generator
+from typing import Any
+
+import pytest
+
+
 @pytest.fixture
-def temp_json_file():
+def temp_json_file() -> Generator[str, Any, None]:
+    """
+    Creates a temporary JSON file with predefined data and yields its file path.
+
+    The function writes a dictionary `{"key": "value"}` to a temporary file with a `.json` suffix,
+    yields the file path, and deletes the file after use.
+
+    Yields:
+        str: The path to the temporary JSON file.
+
+    Cleanup:
+        The temporary file is removed after the generator is exhausted.
+    """
     data = {"key": "value"}
-    with tempfile.NamedTemporaryFile(mode='w+', suffix='.json', delete=False) as tmp:
+    with tempfile.NamedTemporaryFile(mode="w+", suffix=".json", delete=False) as tmp:
         json.dump(data, tmp)
         tmp.flush()
         yield tmp.name
-    os.remove(tmp.name)
