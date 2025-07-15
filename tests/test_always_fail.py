25e41b9c455d010efc0616e2f0ff2d583278de9b
KrijnvanderBurg <32356726+KrijnvanderBurg@users.noreply.github.com>
2025-07-15 13:57:36 +0000

    w

--- tests/test_always_fail.py
+++ tests/test_always_fail.py
@@ -1,2 +1,8 @@
-# def test_always_fail() -> None:
-#     assert False
+"""Test tests"""
+
+
+def test_always_fail() -> None:
+    """
+    Test that always fails by asserting False.
+    """
+    assert False
--- tests/test_always_pass.py
+++ tests/test_always_pass.py
@@ -1,17 +1,8 @@
-from hello_world.__main__ import main
+"""Test tests"""
 
 
-def test_always_pass1() -> None:
+def test_always_pass() -> None:
+    """
+    Test that always passes by asserting True.
+    """
     assert True
-
-
-def test_always_pass2() -> None:
-    assert True
-
-
-def test_always_pass3() -> None:
-    assert True
-
-
-def test_main() -> None:
-    main(filepath="path/to/test.txt")
